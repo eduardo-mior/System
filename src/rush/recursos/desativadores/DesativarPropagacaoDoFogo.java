d17a227aa91d33e195da22485a6fc67d45a3db22
uRyanxDee <72111090+uRyanxD@users.noreply.github.com>
2021-03-10 16:06:48 -0300

    Update DesativarPropagacaoDoFogo.java

--- src/rush/recursos/desativadores/DesativarPropagacaoDoFogo.java
+++ src/rush/recursos/desativadores/DesativarPropagacaoDoFogo.java
@@ -1,24 +1,13 @@
 package rush.recursos.desativadores;
 
-import org.bukkit.event.block.BlockBurnEvent;
-import org.bukkit.event.block.BlockIgniteEvent;
-import org.bukkit.event.block.BlockIgniteEvent.IgniteCause;
 import org.bukkit.event.EventHandler;
-import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockSpreadEvent;
 
 public class DesativarPropagacaoDoFogo implements Listener {
 
-	@EventHandler(priority = EventPriority.LOWEST, ignoreCancelled = true)
-	public void aoPegarFogo(BlockBurnEvent e) {
-		e.setCancelled(true);
-	}
-
-	@EventHandler(priority = EventPriority.LOWEST, ignoreCancelled = true)
-	public void aoEspalharFogo(BlockIgniteEvent e) {
-		if (e.getCause() == IgniteCause.LAVA || e.getCause() == IgniteCause.SPREAD) {
-			e.setCancelled(true);
-		}
-	}
-	
+    @EventHandler
+    public void onSpread(BlockSpreadEvent e) {
+        e.setCancelled(true);
+    }
 }
