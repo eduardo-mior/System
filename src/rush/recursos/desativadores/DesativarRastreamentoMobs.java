b110f3270063b7d98f94a2b8bf743bacd7ff5f35
uRyanxDee <72111090+uRyanxD@users.noreply.github.com>
2021-03-10 16:32:24 -0300

    Create DesativarRastreamentoMobs.java

---
+++ src/rush/recursos/desativadores/DesativarRastreamentoMobs.java
@@ -0,0 +1,19 @@
+package com.factions.utils;
+
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.EntityType;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityTargetEvent;
+
+public class DesativarRastreamentoMobs implements Listener {
+
+    @EventHandler
+    public void onTarget(EntityTargetEvent e) {
+        Entity entityTarget = e.getTarget();
+
+        if (entityTarget.getType() == EntityType.PLAYER) {
+            e.setCancelled(true);
+        }
+    }
+}
