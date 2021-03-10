9e25caebd59921ff110aad5afe8db63021aa1ce6
uRyanxDee <72111090+uRyanxD@users.noreply.github.com>
2021-03-10 16:18:00 -0300

    Update DesativarQuedaDasFolhas.java

--- src/rush/recursos/desativadores/DesativarQuedaDasFolhas.java
+++ src/rush/recursos/desativadores/DesativarQuedaDasFolhas.java
@@ -1,15 +1,13 @@
 package rush.recursos.desativadores;
 
 import org.bukkit.event.EventHandler;
-import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.LeavesDecayEvent;
 
-public class DesativarQuedaDasFolhas implements Listener {
+public class LeavesDecayListener implements Listener {
 
-	@EventHandler(priority = EventPriority.HIGH, ignoreCancelled = true)
-	public void aoCairFolha(LeavesDecayEvent e) {
-		e.setCancelled(true);
-	}
-	
+    @EventHandler
+    public void onDecay(LeavesDecayEvent e) {
+        e.setCancelled(true);
+    }
 }
