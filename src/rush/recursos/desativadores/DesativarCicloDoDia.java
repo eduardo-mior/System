816b1d75280c72a6708c23cf5cf9c107c747b0cd
uRyanxDee <72111090+uRyanxD@users.noreply.github.com>
2021-03-10 16:22:57 -0300

    Update DesativarCicloDoDia.java

--- src/rush/recursos/desativadores/DesativarCicloDoDia.java
+++ src/rush/recursos/desativadores/DesativarCicloDoDia.java
@@ -2,34 +2,13 @@ package rush.recursos.desativadores;
 
 import org.bukkit.Bukkit;
 import org.bukkit.World;
-import org.bukkit.scheduler.BukkitRunnable;
-
-import rush.Main;
 
 public class DesativarCicloDoDia {
 
-	public static void stopDaylightCycle() {
-		new BukkitRunnable() {
-			@Override
-			public void run() {
-				for (World w : Bukkit.getWorlds()) {
-					w.setGameRuleValue("doDaylightCycle", "false");
-					w.setTime(6000);
-				}
-			}
-		}.runTaskLater(Main.get(), 600L);
-	}
-	
-
-	public static void stopDaylightCycleOLD() {
-		new BukkitRunnable() {
-			@Override
-			public void run() {
-				for (World w : Bukkit.getWorlds()) {
-					w.setTime(6000);
-				}
-			}
-		}.runTaskTimer(Main.get(), 600L, 600L);
-	}
-	
+    public static void stopDaylightCycle() {
+        for (World worlds : Bukkit.getWorlds()) {
+            worlds.setGameRuleValue("doDaylightCycle", "false");
+            worlds.setTime(6000);
+        }
+    }
 }
