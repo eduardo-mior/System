a8d60e9dc0adb6fd800f64df4eb6cff2b555dae6
uRyanxDee <72111090+uRyanxD@users.noreply.github.com>
2021-03-10 16:14:29 -0300

    Update DesativarQuedaDaBigorna.java

--- src/rush/recursos/desativadores/DesativarQuedaDaBigorna.java
+++ src/rush/recursos/desativadores/DesativarQuedaDaBigorna.java
@@ -1,26 +1,23 @@
 package rush.recursos.desativadores;
 
 import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityChangeBlockEvent;
 
-public class DesativarQuedaDaBigorna implements Listener {
+public class SandChangeListener implements Listener {
 
-	/**
-	 * Código criado por iPyronic
-	 * Link: https://www.spigotmc.org/threads/prevent-sand-from-falling-upon-placing-sand.133386/
-	 */
-	
-	@EventHandler(ignoreCancelled = true)
-	public void aoCair(EntityChangeBlockEvent e) {
-		if (e.getEntityType() == EntityType.FALLING_BLOCK && e.getTo() == Material.AIR) {
-			if (e.getBlock().getType() == Material.ANVIL) {
-				e.setCancelled(true);
-				e.getBlock().getState().update(false, false);
-			}
-		}
-	}
-	
+    @EventHandler
+    public void onChange(EntityChangeBlockEvent e) {
+        Block block = e.getBlock();
+        Entity entity = e.getEntity();
+
+        if (entity.getType() == EntityType.FALLING_BLOCK & block.getType() == Material.ANVIL) {
+            e.setCancelled(true);
+            block.getState().update(false, false);
+        }
+    }
 }
