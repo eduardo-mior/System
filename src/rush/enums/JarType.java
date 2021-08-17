f5669386b4d75263b960ed31149d2c127670da16
Eduardo Mior <eduardo-mior@hotmail.com>
2021-08-17 15:44:11 -0300

    Altera maneira como era detectada o tipo da JAR do servidor para funcionar corretamente com o minecraft 1.17

--- src/rush/enums/JarType.java
+++ src/rush/enums/JarType.java
@@ -15,20 +15,20 @@ public enum JarType {
 	
 	public static JarType getJarType() {
 		String ver = Bukkit.getVersion();
-		if (ver.contains("git-Torch"))
+		if (ver.contains("-Torch"))
 			return JarType.TORCH;
-		else if (ver.contains("git-Purpur"))
-			return JarType.PURPUR;
-		else if (ver.contains("git-Taco"))
-			return JarType.TACO_SPIGOT;
-		else if (ver.contains("git-Paper"))
-			return JarType.PAPER_SPIGOT;
-		else if (ver.contains("git-Spigot"))
-			return JarType.SPIGOT;
-		else if (ver.contains("git-Bukkit"))
-			return JarType.BUKKIT;
-		else if (ver.contains("git-Cauldron"))
+		else if (ver.contains("-Cauldrons"))
 			return JarType.CAULDRON;
+		else if (ver.contains("-Purpur"))
+			return JarType.PURPUR;
+		else if (ver.contains("-Taco"))
+			return JarType.TACO_SPIGOT;
+		else if (ver.contains("-Paper"))
+			return JarType.PAPER_SPIGOT;
+		else if (ver.contains("-Spigot"))
+			return JarType.SPIGOT;
+		else if (ver.contains("-Bukkit"))
+			return JarType.BUKKIT;
 		else
 			return JarType.DESCONHECIDA;
 	}
