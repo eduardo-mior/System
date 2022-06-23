62f04c96abb9ae2cfd1d26aa520fd7dfaa1e9713
Eduardo Mior <eduardo-mior@hotmail.com>
2022-06-23 18:00:28 -0300

    Ajusta para que todas as configs sejam lidas em UTF-8

--- src/rush/utils/manager/ConfigManager.java
+++ src/rush/utils/manager/ConfigManager.java
@@ -1,6 +1,9 @@
 package rush.utils.manager;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
 
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
@@ -16,9 +19,15 @@ public class ConfigManager {
 	}
 	
 	public static FileConfiguration getConfig(String file) {
-      	File arquivo = new File(Main.get().getDataFolder() + File.separator + file + ".yml");
-      	FileConfiguration config = (FileConfiguration)YamlConfiguration.loadConfiguration(arquivo);
-      	return config;
+      	try {
+      		File arquivo = new File(Main.get().getDataFolder() + File.separator + file + ".yml");
+			InputStreamReader arquivoStream = new InputStreamReader(new FileInputStream(arquivo), Charset.forName("UTF-8").name());
+			FileConfiguration config = (FileConfiguration)YamlConfiguration.loadConfiguration(arquivoStream);
+			return config;
+		} catch (Throwable e) {
+			e.printStackTrace();
+		} 
+      	return null;
 	}
 	
 }
